cec5b957fe7c9b6d262c94a8b4d03ea0a21337e7
giho <giho7427@daum.net>
2023-12-14 23:31:36 +0900

    [fix] sign 페이지 스크롤바 제거

--- signin.js
+++ signin.js
@@ -8,28 +8,34 @@ const signinPasswordError = document.querySelector('.form-sign__input-error.pass
 const signinEmailHandler = function (event) {
     const emailAddress = event.target.value;
     const confirmEmail = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
-    if (!emailAddress) {
-        signinEmailError.textContent = '이메일을 입력해주세요'
-    } else if (!confirmEmail.test(emailAddress)) {
-        signinEmailError.textContent = '올바른 이메일 주소가 아닙니다'
+    if (!emailAddress) { // 이메일 주소가 없는 경우
+        signinEmailError.textContent = '이메일을 입력해주세요';
+    } else if (!confirmEmail.test(emailAddress)) { // 이메일 형식이 아닌경우
+        signinEmailError.textContent = '올바른 이메일 주소가 아닙니다';
+    } else { // 이메일 형식인 경우 경고창을 띄우지 않는다
+        signinEmailError.textContent = '';
     }
 }
 
 const signinPasswordHandler = function (event) { // 패스워드 인풋 관련 함수
     const password = event.target.value;
-    if (!password) signinPasswordError.textContent = '비밀번호를 확인해주세요'
+    if (!password) { // 비밀번호를 입력 안한 경우
+        signinPasswordError.textContent = '비밀번호를 확인해주세요';
+    } else { // 비밀번호를 입력한 경우
+        signinPasswordError.textContent = '';
+    }
 }
 
 const signinLoginBtnHandler = function (event) { // 로그인 버튼을 눌렀을 때 함수
-    event.preventDefault();
     if (signinEmailInput.value !== 'test@codeit.com' && signinPasswordInput.value !== 'codeit101') {
+        event.preventDefault();
         signinEmailError.textContent = '이메일을 확인해주세요';
         signinPasswordError.textContent = '비밀번호를 확인해주세요';
-    }
-    if (!signinEmailInput.value) {
+    } else if (!signinEmailInput.value) {
+        event.preventDefault();
         signinEmailError.textContent = '이메일을 입력해주세요';
-    }
-    if (!signinPasswordInput.value) {
+    } else if (!signinPasswordInput.value) {
+        event.preventDefault();
         signinPasswordError.textContent = '비밀번호를 입력해주세요';
     }
 }
