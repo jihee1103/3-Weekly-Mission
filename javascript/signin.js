c101206639ad32eec8c63082b507ffebd9a9a8a2
jiyong0106 <parkjiy96@naver.com>
2023-12-15 22:25:56 +0900

    edit code

--- javascript/signin.js
+++ javascript/signin.js
@@ -6,60 +6,36 @@ const password_error = document.querySelector('.password_error');
 const input_line = document.querySelector('.input_line');
 const logbtn = document.querySelector('.logbtn');
 
+//이메일
+email.addEventListener('focusout', email_check)
+function email_check(){
+  if(email.value===""){
+    email.classList.add('input_line');
+    email_error.innerText='이메일을 입력해주세요';
+  }else if(!email.value.includes('@')){
+    email.classList.add('input_line')
+    email_error.innerText='올바른 이메일 주소가 아닙니다.'
+  }else{
+    email.classList.remove('input_line');
+    email_error.innerText='';
+  };
+};
+email.addEventListener('focusout', email_check)
 
-// 이메일 입력안하면 경고뜨는 함수
+// 비밀번호 없으면 경고하는 함수
 
-// function email_check(){
-//     if(email.value === ""){
-//   email.classList.add('input_line');
-//     email_error.innerText='이메일을 입력해주세요';
-//   }else{
-//     email.classList.remove('input_line');
-//     email_error.innerText='';
-//   };
-// };
-// email.addEventListener('focusout', email_check);
+function password_check(){
+  if(password.value === ""){
+    password.classList.add('input_line');
+    password_error.innerText = '비밀번호를 입력해주세요';
+  }else{
+    password.classList.remove('input_line');
+    password_error.innerText = '';
+  };
+};
+password.addEventListener('focusout', password_check);
 
-//@ 안쓰면 경고뜨는 함수
-
-// function email_contains(){
-//   if(!email.value.includes('@')){
-//   email.classList.add('input_line');
-//     email_error.innerText='올바른 이메일 주소가 아닙니다';
-//   }else{
-//     email.classList.remove('input_line');
-//     email_error.innerText='';
-//   };
-// };
-// email.addEventListener('focusout', email_contains);
-
-//이메일 비교해서 틀린거 알려주는 함수
-
-// function email_compare(){
-//   if(email.value === 'test@codeit.com'){
-//     email.classList.add('input_line');
-//     email_error.innerText ='이미 사용 중인 이메일입니다.';
-//   }else{
-//     email.classList.remove('input_line');
-//     email_error.innerText ='';
-//   };
-// };
-// email.addEventListener('focusout', email_compare);
-
-//비밀번호 없으면 경고하는 함수
-
-// function password_check(){
-//   if(password.value === ""){
-//     password.classList.add('input_line');
-//     password_error.innerText = '비밀번호를 입력해주세요';
-//   }else{
-//     password.classList.remove('input_line');
-//     password_error.innerText = '';
-//   };
-// };
-// password.addEventListener('focusout', password_check);
-
-//이메일이랑 비밀번호 없으면 로그인 안되고 경고하는 함수
+// 이메일이랑 비밀번호 없으면 로그인 안되고 경고하는 함수
 
 function logbtn_check(){
   if(email.value === "" && password.value === ""){
@@ -72,15 +48,52 @@ function logbtn_check(){
 };
 logbtn.addEventListener('click', logbtn_check);
 
-//로그인시 folder 페이지로 이동하는 함수
+// 로그인시 folder 페이지로 이동하는 함수
 
-function folder_page (){
+function folder_page(){
   if(email.value === 'test@codeit.com' && password.value === 'codeit101'){
-    window.location.href = '../html/folder.html'
+    window.location.href = '../html/folder.html';
   };
 };
 logbtn.addEventListener('click', folder_page);
 
+// 이메일 입력안하면 경고뜨는 함수
+
+// function email_check(){
+//     if(email.value === ""){
+//     email.classList.add('input_line');
+//     email_error.innerText='이메일을 입력해주세요';
+//   }else{
+//     email.classList.remove('input_line');
+//     email_error.innerText='';
+//   };
+// };
+// email.addEventListener('focusout', email_check);
+
+// // @ 안쓰면 경고뜨는 함수
+
+// function email_contains(){
+//   if(email.value !== ""){
+//   if(!email.value.includes('@')){
+//   email.classList.add('input_line');
+//     email_error.innerText='올바른 이메일 주소가 아닙니다';
+//   }else{
+//     email.classList.remove('input_line');
+//     email_error.innerText='';
+//   };
+// }
+// };
+// email.addEventListener('focusout', email_contains);
 
 
-//onchange
+
+//이벤트 위임으로 처리해보기
+
+// const inputlist = document.querySelectorAll('input');
+// inputlist.forEach((input) =>{ 
+// input.addEventListener('focusout', function(){
+//   if(email.value === ""){
+//       email.classList.add('input_line');
+//       email_error.innerText='이메일을 입력해주세요';
+//     }
+// })
--- javascript/signup.js
+++ javascript/signup.js
@@ -14,48 +14,99 @@ const input_line = document.querySelector('.input_line');
 //패스워드 정규식
 let reg = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/
 
-// 이메일 입력 안하면 경고뜨는 함수
+//이메일 확인
+function email_check(){
+  if(email.value === 'test@codeit.com'){
+    email.classList.add('input_line');
+    email_error.innerText ='이미 사용 중인 이메일입니다.';
+  }else if(email.value === ""){
+    email.classList.add('input_line');
+    email_error.innerText='이메일을 입력해주세요'
+  }else if(!email.value.includes('@')){
+    email.classList.add('input_line');
+    email_error.innerText='올바른 이메일 주소가 아닙니다'
+  }else{
+    email.classList.remove('input_line');
+    email_error.innerText='';
+  }
+}
+email.addEventListener('focusout', email_check);
 
-// function email_check(e){
-//     if(email.value === ""){
-//   email.classList.add('input_line');
-//     email_error.innerText='이메일을 입력해주세요'
-//   }else{
-//     email.classList.remove('input_line');
-//     email_error.innerText=''
-//   };
-// };
-// email.addEventListener('focusout', email_check);
+// 패스워드 확인
+function password_check(){
+  if(password.value === ""){
+    password.classList.add('input_line');
+    password_error.innerText = '비밀번호를 입력해주세요';
+  }else if(password.value!==""){
+    if(!reg.test(password.value)){
+      password.classList.add('input_line')
+      password_error.innerText = '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요';
+    }
+  else{ 
+    password.classList.remove('input_line');
+    password_error.innerText = '';
+  }
+  }else{
+    password.classList.remove('input_line');
+    password_error.innerText = '';
+  }
+};
 
-
-//@ 안쓰면 경고뜨는 함수
-
-
-// function email_contains(){
-//   if(!email.value.includes('@')){
-//   email.classList.add('input_line');
-//     email_error.innerText='올바른 이메일 주소가 아닙니다';
-//   }else{
-//     email.classList.remove('input_line');
-//     email_error.innerText='';
-//   };
-// };
-// email.addEventListener('focusout', email_contains);
-
+//패스워드 확인 2
+  function password_check2(){
+    if(password.value !== password2.value){
+      password_error2.innerText = '비밀번호가 일치하지 않아요';
+      password2.classList.add('input_line')
+  }else{
+    password_error2.innerText = '';
+    password2.classList.remove('input_line');
+  };
+};
+password.addEventListener('focusout', password_check)
+password2.addEventListener('focusout', password_check2)
 
 
 //이메일 비교해서 중복 경고하는  함수
 
-function email_compare(){
-  if(email.value === 'test@codeit.com'){
-    email.classList.add('input_line');
-    email_error.innerText ='이미 사용 중인 이메일입니다.';
-  }else{
-    email.classList.remove('input_line');
-    email_error.innerText ='';
-  };
-};
-email.addEventListener('focusout', email_compare);
+// function email_compare(){
+//   if(email.value === 'test@codeit.com'){
+//       email.classList.add('input_line');
+//       email_error.innerText ='이미 사용 중인 이메일입니다.';
+//   }else{
+//       email.classList.remove('input_line');
+//       email_error.innerText ='';
+//   };console.log(email_compare)
+// };
+// email.addEventListener('focusout', email_compare);
+
+// // 이메일 입력 안하면 경고뜨는 함수
+
+// function email_check(){
+//     if(email.value === ""){
+//       email.classList.add('input_line');
+//       email_error.innerText='이메일을 입력해주세요'
+//   }else{
+//       email.classList.remove('input_line');
+//       email_error.innerText='';
+//   };
+//   console.log(email_check)
+// };
+// email.addEventListener('focusout', email_check);
+
+// //@ 안쓰면 경고뜨는 함수
+
+// function email_contains(){
+//     if(email.value!==""){
+//     if(!email.value.includes('@')){
+//     email.classList.add('input_line');
+//       email_error.innerText='올바른 이메일 주소가 아닙니다';
+//     }else{
+//       email.classList.remove('input_line');
+//       email_error.innerText='';
+//     }; console.log(email_contains)
+//   };
+// };
+// email.addEventListener('focusout', email_contains);
 
 // 비밀번호 없으면 경고해주는 함수
 
@@ -70,7 +121,7 @@ email.addEventListener('focusout', email_compare);
 // };
 // password.addEventListener('focusout', password_check);
 
-//비밀번호랑 비밀번호 확인이랑 비교해서 경고하는 함수
+// // //비밀번호랑 비밀번호 확인이랑 비교해서 경고하는 함수
 
 // function password_compare(){
 //   if(password.value !== password2.value){
@@ -83,13 +134,15 @@ email.addEventListener('focusout', email_compare);
 // };
 // password2.addEventListener('focusout', password_compare);
 
-//비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요 경고문 함수
+// // //비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요 경고문 함수
 
 // function password_length(){
-//   if(!reg.test(password.value)){
-//     password_error.innerText = '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요';
-//   }else{
-//     password_error.innerText = '';
+//     if(password.value!==""){
+//     if(!reg.test(password.value)){
+//       password_error.innerText = '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요';
+//     }else{
+//       password_error.innerText = '';
+//     };
 //   };
 // };
 // password.addEventListener('focusout', password_length);
