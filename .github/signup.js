bac04b46f1631a588f2756e2c102e3195019be80
괴도소연\박소연 <rnlduq157@naver.com>
2023-12-18 08:39:55 +0900

    refactor: 모듈화 및 코드 재정렬
    - 공통으로 사용하는 코드를 validation.js 파일로 추출
    - 로그인 페이지에도 적용할 수 있도록 html 파일에 id 추가

--- .github/signup.js
+++ .github/signup.js
@@ -1,11 +1,7 @@
-const testEmail = "test@codeit.com";
-const testPassword = "codeit101";
-
-const emailInput = document.querySelector('#email');
-const emailError = document.querySelector('#email-error');
 
 function emailErrorMessage() {
   const inputValue = emailInput.value;
+
   if (inputValue === '') {
     showEmailError('이메일을 입력해주세요.');
   } else if (inputValue === testEmail) {
@@ -13,40 +9,24 @@ function emailErrorMessage() {
   } else if (!isValidEmail(inputValue)) {
     showEmailError('올바른 이메일 주소가 아닙니다.');
   } else {
-    hideError();
+    hideEmailError();
   }
-}
-
-function isValidEmail(email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-}
-
-function showEmailError(message) {
-  emailError.textContent = message;
-  emailError.classList.add('show-error', 'error-message');
-  emailInput.classList.add('error');
-}
-
-function hideError() {
-  emailError.textContent = '';
-  emailError.classList.remove('show-error', 'error-message');
-  emailInput.classList.remove('error');
 }
 
 emailInput.addEventListener('focusout', emailErrorMessage);
 
 /*비밀번호 오류*/
 
-const pwInput = document.querySelector('#password');
+
 const pw2Input = document.querySelector('#password2');
-const pwError = document.querySelector('#pw-error');
 const pw2Error = document.querySelector('#pw2-error')
 
 function pwErrorMessage() {
   const inputValue = pwInput.value;
   const targetValue = pw2Input.value;
 
-  hideError();
+  hidePasswordError();
+  hidePassword2Error();
 
   if (inputValue === '') {
     showPasswordError('비밀번호를 입력해주세요.');
@@ -57,27 +37,13 @@ function pwErrorMessage() {
   }
 }
 
-function isValidPw(password) {
-  return password.length >= 8 && /[a-zA-Z]/.test(password) && /\d/.test(password);
-}
-
-function showPasswordError(message) {
-  pwError.textContent = message;
-  pwError.classList.add('show-error', 'error-message');
-  pwInput.classList.add('error');
-}
-
 function showNotMatchError(message) {
   pw2Error.textContent = message;
   pw2Error.classList.add('show-error', 'error-message');
   pw2Input.classList.add('error');
 }
 
-function hideError() {
-  pwError.textContent = '';
-  pwError.classList.remove('show-error');
-  pwInput.classList.remove('error');
-
+function hidePassword2Error() {
   pw2Error.textContent = '';
   pw2Error.classList.remove('show-error');
   pw2Input.classList.remove('error');
---
+++ .github/validation..js
@@ -0,0 +1,38 @@
+const testEmail = "test@codeit.com";
+const testPassword = "codeit101";
+const emailInput = document.querySelector('#email');
+const emailError = document.querySelector('#email-error');
+const pwInput = document.querySelector('#password');
+const pwError = document.querySelector('#pw-error');
+
+function isValidEmail(email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function isValidPw(password) {
+  return password.length >= 8 && /[a-zA-Z]/.test(password) && /\d/.test(password);
+}
+
+function showEmailError(message) {
+  emailError.textContent = message;
+  emailError.classList.add('show-error', 'error-message');
+  emailInput.classList.add('error');
+}
+
+function hideEmailError() {
+  emailError.textContent = '';
+  emailError.classList.remove('show-error', 'error-message');
+  emailInput.classList.remove('error');
+}
+
+function showPasswordError(message) {
+  pwError.textContent = message;
+  pwError.classList.add('show-error', 'error-message');
+  pwInput.classList.add('error');
+}
+
+function hidePasswordError() {
+  pwError.textContent = '';
+  pwError.classList.remove('show-error');
+  pwInput.classList.remove('error');
+}
