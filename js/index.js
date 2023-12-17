fbbd8bdb4047deb15a68d8fbd54215d550477c43
jihee <sweet9811@gmail.com>
2023-12-17 17:27:41 +0900

    feat: 로그인 버튼 클릭시 이벤트 추가

--- js/index.js
+++ js/index.js
@@ -4,6 +4,9 @@ const passInput = document.querySelector("#password");
 const inputs = document.querySelectorAll(".input");
 const errorMessage = document.querySelectorAll(".message");
 const secondPassInput = document.querySelector("#check-password");
+const signinbtn = document.querySelector(".signin-confirm");
+const signupbtn = document.querySelector(".signup-confirm");
+let link = "/folder.html";
 
 //이메일 에러
 function validEmail(e) {
@@ -45,6 +48,24 @@ function signupPassChecker(e) {
   }
 }
 
+function signinConfirm(e) {
+  if (
+    emailInput.value === "test@codeit.com" &&
+    passInput.value === "codeit101"
+  ) {
+    location.href = link;
+  } else {
+    errorMessage[0].textContent = "이메일을 확인해주세요.";
+    errorMessage[0].classList.add("error");
+    emailInput.classList.add("error-border");
+
+    errorMessage[1].textContent = "비밀번호를 확인해주세요.";
+    errorMessage[1].classList.add("error");
+    passInput.classList.add("error-border");
+  }
+}
+
 emailInput.addEventListener("focusout", validEmail);
 passInput.addEventListener("focusout", passChecker);
 secondPassInput.addEventListener("focusout", signupPassChecker);
+signinbtn.addEventListener("click", signinConfirm);
