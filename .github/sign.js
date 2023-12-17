19fed7938cb6877162b3d67eda374af3829e1d32
괴도소연\박소연 <rnlduq157@naver.com>
2023-12-18 07:57:32 +0900

    feat: 회원가입 페이지 내 비밀번호 이벤트 추가 및 html 수정
    - 비밀번호 에러메세지 위치 오류가 <div> 태그 원인임을 찾아 수정

--- .github/sign.js
+++ .github/sign.js
@@ -34,3 +34,54 @@ function hideError() {
 }
 
 emailInput.addEventListener('focusout', emailErrorMessage);
+
+/*비밀번호 오류*/
+
+const pwInput = document.querySelector('#password');
+const pw2Input = document.querySelector('#password2');
+const pwError = document.querySelector('#pw-error');
+const pw2Error = document.querySelector('#pw2-error')
+
+function pwErrorMessage() {
+  const inputValue = pwInput.value;
+  const targetValue = pw2Input.value;
+
+  hideError();
+
+  if (inputValue === '') {
+    showPasswordError('비밀번호를 입력해주세요.');
+  } else if (!isValidPw(inputValue)) {
+    showPasswordError('비밀번호는 영문,숫자 조합 8자 이상 입력해 주세요.');
+  } else if (inputValue !== targetValue) {
+    showNotMatchError('비밀번호가 일치하지 않아요.');
+  }
+}
+
+function isValidPw(password) {
+  return password.length >= 8 && /[a-zA-Z]/.test(password) && /\d/.test(password);
+}
+
+function showPasswordError(message) {
+  pwError.textContent = message;
+  pwError.classList.add('show-error', 'error-message');
+  pwInput.classList.add('error');
+}
+
+function showNotMatchError(message) {
+  pw2Error.textContent = message;
+  pw2Error.classList.add('show-error', 'error-message');
+  pw2Input.classList.add('error');
+}
+
+function hideError() {
+  pwError.textContent = '';
+  pwError.classList.remove('show-error');
+  pwInput.classList.remove('error');
+
+  pw2Error.textContent = '';
+  pw2Error.classList.remove('show-error');
+  pw2Input.classList.remove('error');
+}
+
+pwInput.addEventListener('focusout', pwErrorMessage);
+pw2Input.addEventListener('focusout', pwErrorMessage);
