3c23c07a8c35645af60b54a5d4b47788de7facc1
jinyoung1018 <jywsb1816@gmail.com>
2023-12-21 01:15:31 +0900

    변수명 변경

--- script/signin.js
+++ script/signin.js
@@ -1,7 +1,7 @@
 const inputEmail = document.querySelector("#inputEmail");
-const inputPw = document.querySelector("#inputPw");
+const inputPassword = document.querySelector("#inputPw");
 const emailError = document.querySelector("#email_err");
-const pwError = document.querySelector("#pw_err");
+const passwordError = document.querySelector("#pw_err");
 const signinForm = document.querySelector("form");
 const inputPwIcon = document.querySelector("#inputPwIcon");
 
@@ -22,16 +22,16 @@ function checkEmail() {
 
 function checkPassword() {
     const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$/;
-    if (!inputPw.value) {
-        pwError.textContent = "비밀번호를 입력해주세요.";
-        inputPw.className = "error-box";
-    } else if (!passwordRegex.test(inputPw.value)) {
-        pwError.textContent =
+    if (!inputPassword.value) {
+        passwordError.textContent = "비밀번호를 입력해주세요.";
+        inputPassword.className = "error-box";
+    } else if (!passwordRegex.test(inputPassword.value)) {
+        passwordError.textContent =
             "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
-        inputPw.className = "error-box";
+        inputPassword.className = "error-box";
     } else {
-        pwError.textContent = " ";
-        inputPw.className = "inputbox";
+        passwordError.textContent = " ";
+        inputPassword.className = "inputbox";
     }
 }
 
@@ -39,31 +39,31 @@ function signIn(event) {
     event.preventDefault();
     if (
         inputEmail.value === "test@codeit.com" &&
-        inputPw.value === "codeit101"
+        inputPassword.value === "codeit101"
     ) {
         location.href = "/folder";
     } else {
         emailError.textContent = "이메일을 확인해주세요.";
-        pwError.textContent = "비밀번호를 확인해주세요.";
+        passwordError.textContent = "비밀번호를 확인해주세요.";
     }
 }
 
-function showPasswordToggle(element_icon, element_input) {
+function showPasswordToggle(elementIcon, elementInput) {
     const img_path = "../images/eye-off.png";
 
-    if (element_input.type == "text") {
-        element_input.type = "password";
-        element_icon.src = "../images/eye-off.png";
+    if (elementInput.type == "text") {
+        elementInput.type = "password";
+        elementIcon.src = "../images/eye-off.png";
     } else {
-        element_input.type = "text";
-        element_icon.src = "../images/eye-on.png";
+        elementInput.type = "text";
+        elementIcon.src = "../images/eye-on.png";
     }
 }
 
 inputPwIcon.addEventListener("click", () => {
-    showPasswordToggle(inputPwIcon, inputPw);
+    showPasswordToggle(inputPwIcon, inputPassword);
 });
 
 inputEmail.addEventListener("focusout", checkEmail);
-inputPw.addEventListener("focusout", checkPassword);
+inputPassword.addEventListener("focusout", checkPassword);
 signinForm.addEventListener("submit", signIn);
--- script/signup.js
+++ script/signup.js
@@ -1,24 +1,24 @@
 const inputEmail = document.querySelector("#inputEmail");
-const inputPw = document.querySelector("#inputPw");
-const inputPwConfirm = document.querySelector("#inputPwConfirm");
+const inputPassword = document.querySelector("#inputPw");
+const inputPasswordConfirm = document.querySelector("#inputPwConfirm");
 const emailError = document.querySelector("#email_err");
-const pwError = document.querySelector("#pw_err");
-const pwConfirmError = document.querySelector("#pwConfirm_err");
+const passwordError = document.querySelector("#pw_err");
+const passwordConfirmError = document.querySelector("#pwConfirm_err");
 const signupForm = document.querySelector("form");
-const inputPwIcon = document.querySelector("#inputPwIcon");
-const inputPwComfirmIcon = document.querySelector("#inputPwComfirmIcon");
+const inputPasswordIcon = document.querySelector("#inputPwIcon");
+const inputPasswordComfirmIcon = document.querySelector("#inputPwComfirmIcon");
 
 let emailValid = false;
 let passwordValid = false;
 let passwordConfirmValid = false;
 
 function checkEmail() {
-    const email_format =
+    const EMAIL_REGEX =
         /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
     if (!inputEmail.value) {
         emailError.textContent = "이메일을 입력해주세요.";
         inputEmail.className = "error-box";
-    } else if (!email_format.test(inputEmail.value)) {
+    } else if (!EMAIL_REGEX.test(inputEmail.value)) {
         emailError.textContent = "올바른 이메일 주소가 아닙니다.";
         inputEmail.className = "error-box";
     } else if (inputEmail.value === "test@codeit.com") {
@@ -32,29 +32,29 @@ function checkEmail() {
 
 function checkPassword() {
     const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,16}$/;
-    if (!inputPw.value) {
-        pwError.textContent = "비밀번호를 입력해주세요.";
-        inputPw.className = "error-box";
-    } else if (!passwordRegex.test(inputPw.value)) {
-        pwError.textContent =
+    if (!inputPassword.value) {
+        passwordError.textContent = "비밀번호를 입력해주세요.";
+        inputPassword.className = "error-box";
+    } else if (!passwordRegex.test(inputPassword.value)) {
+        passwordError.textContent =
             "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
-        inputPw.className = "error-box";
-    } else if (inputPw.value && passwordRegex.test(inputPw.value)) {
-        pwError.textContent = " ";
-        inputPw.className = "inputbox";
+        inputPassword.className = "error-box";
+    } else if (inputPassword.value && passwordRegex.test(inputPassword.value)) {
+        passwordError.textContent = " ";
+        inputPassword.className = "inputbox";
         passwordValid = true;
     }
 }
 
 function passwordConfirm() {
-    if (!inputPwConfirm.value) {
-        pwConfirmError.textContent = "비밀번호를 입력해주세요.";
-        inputPwConfirm.className = "error-box";
-    } else if (inputPwConfirm.value !== inputPw.value) {
-        pwConfirmError.textContent = "비밀번호가 일치하지 않아요.";
+    if (!inputPasswordConfirm.value) {
+        passwordConfirmError.textContent = "비밀번호를 입력해주세요.";
+        inputPasswordConfirm.className = "error-box";
+    } else if (inputPasswordConfirm.value !== inputPassword.value) {
+        passwordConfirmError.textContent = "비밀번호가 일치하지 않아요.";
     } else {
-        pwConfirmError.textContent = " ";
-        inputPwConfirm.className = "inputbox";
+        passwordConfirmError.textContent = " ";
+        inputPasswordConfirm.className = "inputbox";
         passwordConfirmValid = true;
     }
 }
@@ -72,36 +72,36 @@ function signUp(event) {
         emailError.textContent = "이메일을 확인해주세요.";
         inputEmail.className = "error-box";
     } else if (passwordValid === false) {
-        pwError.textContent = "비밀번호를 확인해주세요.";
-        inputPw.className = "error-box";
+        passwordError.textContent = "비밀번호를 확인해주세요.";
+        inputPassword.className = "error-box";
     } else if (passwordConfirmValid === false) {
-        pwConfirmError.textContent = "비밀번호를 확인해주세요.";
-        inputPwConfirm.className = "error-box";
+        passwordConfirmError.textContent = "비밀번호를 확인해주세요.";
+        inputPasswordConfirm.className = "error-box";
     }
 }
 
-function showPasswordToggle(element_icon, element_input) {
+function showPasswordToggle(elementIcon, elementInput) {
     const img_path = "../images/eye-off.png";
 
-    if (element_input.type == "text") {
-        element_input.type = "password";
-        element_icon.src = "../images/eye-off.png";
+    if (elementInput.type == "text") {
+        elementInput.type = "password";
+        elementIcon.src = "../images/eye-off.png";
     } else {
-        element_input.type = "text";
-        element_icon.src = "../images/eye-on.png";
+        elementInput.type = "text";
+        elementIcon.src = "../images/eye-on.png";
     }
 }
 
-inputPwIcon.addEventListener("click", () => {
-    showPasswordToggle(inputPwIcon, inputPw);
+inputPasswordIcon.addEventListener("click", () => {
+    showPasswordToggle(inputPasswordIcon, inputPassword);
 });
 
-inputPwComfirmIcon.addEventListener("click", () => {
-    showPasswordToggle(inputPwComfirmIcon, inputPwConfirm);
+inputPasswordComfirmIcon.addEventListener("click", () => {
+    showPasswordToggle(inputPasswordComfirmIcon, inputPasswordConfirm);
 });
 
 inputEmail.addEventListener("focusout", checkEmail);
-inputPw.addEventListener("focusout", checkPassword);
-inputPwConfirm.addEventListener("focusout", passwordConfirm);
+inputPassword.addEventListener("focusout", checkPassword);
+inputPasswordConfirm.addEventListener("focusout", passwordConfirm);
 
 signupForm.addEventListener("submit", signUp);
