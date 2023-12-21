dce3dac3b747207672ec25dba3b54c7ae164d1e2
siyeol97 <leesi2830@naver.com>
2023-12-21 17:42:56 +0900

    feat: 회원가입 시 accessToken 저장, 심화 요구사항 완료

--- utils/signin.js
+++ utils/signin.js
@@ -1,47 +1,36 @@
 import {
   $inputEmail,
   $inputPassword,
-  $inputEmailBox,
-  $inputPasswordBox,
   $loginButton,
   $eyeIcon,
-  displayLoginError,
-  removeLoginError,
   checkInputEmail,
   checkInputPassword,
   toggleDisplayPassword,
+  requestSign,
   displayCheckEmailPassword,
+  saveAccessToken,
 } from "./sign.js";
 
 // 로그인 할 때, 이메일, 비밀번호 검사
 async function checkLogin(e) {
   e.preventDefault();
-
-  const inputUser = {
-    email: $inputEmail.value,
-    password: $inputPassword.value,
-  };
-
   try {
-    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(inputUser),
-    });
-
-    // 리퀘스트 성공
+    const inputEmailPassword = {
+      email: $inputEmail.value,
+      password: $inputPassword.value,
+    };
+    // request 요청
+    const response = await requestSign(
+      "https://bootcamp-api.codeit.kr/api/sign-in",
+      inputEmailPassword
+    );
+    // 리퀘스트 성공 시
     if (response.status === 200) {
       // 로컬 스토리지에 accessToken 저장
-      const result = response.json();
-      result.then((res) => {
-        const accessToken = res.data.accessToken;
-        localStorage.setItem("accessToken", accessToken);
-      });
+      saveAccessToken(response);
       // folder page로 이동
       location.href = "folder.html";
-      // 리퀘스트 실패
+      // 리퀘스트 실패 시
     } else if (response.status === 400) displayCheckEmailPassword();
   } catch (error) {
     alert("오류가 발생했습니다.");
--- utils/signup.js
+++ utils/signup.js
@@ -13,6 +13,8 @@ import {
   checkInputEmail,
   checkInputPassword,
   displayCheckEmailPassword,
+  requestSign,
+  saveAccessToken,
 } from "./sign.js";
 
 // 회원가입 시 비밀번호 값과 확인값 검사
@@ -42,19 +44,15 @@ function checkVaildPassword() {
 
 // 회원가입 시 이메일 중복 검사
 async function checkSignupDuplicateEmail() {
-  const inputEmail = { email: $inputEmail.value };
-
   removeLoginError($inputEmail, ".error-message-email");
-
   try {
-    const emailCheckResponse = await fetch("https://bootcamp-api.codeit.kr/api/check-email", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(inputEmail),
-    });
-
+    const inputEmail = { email: $inputEmail.value };
+    // 이메일 중복 확인 request 요청
+    const emailCheckResponse = await requestSign(
+      "https://bootcamp-api.codeit.kr/api/check-email",
+      inputEmail
+    );
+    // 이메일이 중복이면
     if (emailCheckResponse.status === 409) {
       displayLoginError($inputEmail, $inputEmailBox, "이미 사용중인 이메일 입니다.");
       return true;
@@ -77,22 +75,26 @@ async function checkSignup(e) {
 
   // 모든 값이 유효하면
   if (isValidEmail && isValuePassword && isValidPassword && isCheckedPassword) {
-    const inputSignup = {
-      email: $inputEmail.value,
-      password: $inputPassword.value,
-    };
-
-    const vaildSignup = await fetch("https://bootcamp-api.codeit.kr/api/sign-up", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(inputSignup),
-    });
-    // 회원가입 성공 시
-    if (vaildSignup.status === 200) location.href = "folder.html";
-    else displayCheckEmailPassword();
-  } else displayCheckEmailPassword(); // 유효한 값이 하나라도 있을 때
+    try {
+      const inputEmailPassword = {
+        email: $inputEmail.value,
+        password: $inputPassword.value,
+      };
+      // request 요청
+      const vaildSignup = await requestSign(
+        "https://bootcamp-api.codeit.kr/api/sign-up",
+        inputEmailPassword
+      );
+      // 회원가입 성공 시
+      if (vaildSignup.status === 200) {
+        saveAccessToken(vaildSignup);
+        location.href = "folder.html";
+        // 회원가입 실패 시
+      } else displayCheckEmailPassword();
+    } catch (error) {
+      alert("오류가 발생했습니다.");
+    }
+  } else displayCheckEmailPassword(); // 유효하지 않은 값이 하나라도 있을 때
 }
 
 // 이벤트 리스너 추가
