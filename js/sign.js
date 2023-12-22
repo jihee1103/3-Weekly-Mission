a4b4804d4e8ac5be122f1d7f8108a0b487fc7407
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2023-12-22 17:20:44 +0900

    코드 수정 및 기본과제 추가

--- js/sign.js
+++ js/sign.js
@@ -1,20 +1,26 @@
+import { signValidator, checkPassword, setErrorMessage } from './signValidations.js'
+
 // 이벤트 제거용
 const controller = new AbortController();
 const signal = controller.signal;
 
+//form
+const form = document.querySelector('form');
+
 // input 태그들 
-const inputs = document.querySelectorAll('input');
+const inputs = [...document.querySelectorAll('input')];
 const inputId = document.getElementById('email');
 const inputPw = document.getElementById('password');
 const inputPwRepeat = document.getElementById('password-repeat');
 
 // 에러 메세지 span 태그들
-const spanId = inputId.nextElementSibling;
-const spanPw= inputPw.nextElementSibling;  
+const spanId = document.getElementById('spanId');
+const spanPw = document.getElementById('spanPw');
+const spanPwRe = document.getElementById('spanPwRe');
 
 // 정규 표현식
 const regexId = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-const regexPw =  /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/;
+const regexPw = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,20}$/;
 
 // 눈 아이콘
 const eyes = document.querySelectorAll('.eye');
@@ -22,112 +28,142 @@ const eyes = document.querySelectorAll('.eye');
 // 로그인, 회원가입 버튼
 const signBtn = document.querySelector('.sign-submit-btn');
 
-// 임시 테스트 유저 데이터 
-const user = {
-  email : 'test@codeit.com',
-  password : 'codeit101'
-}
+// api url
+const apiUrl = 'https://bootcamp-api.codeit.kr/'
 
-// focusout 되었을 때 사용 될 에러메세지 
-function inputFocusOut({target}) {
-  const span = target.nextElementSibling;
-  span.textContent ='';
-  if(target.type=='email'){
-    if(target.value===''){
-      span.textContent = '이메일을 입력해주세요.';
-    }else if(!regexId.test(target.value)){
-      span.textContent = '올바른 이메일 주소가 아닙니다.';
-    }
-  }else if(target.id==='password'){
-    if(target.value===''){
-      span.textContent='비밀번호를 입력해주세요.';
-    }else if(!regexPw.test(target.value)){
-      span.textContent = '비밀번호는 영문, 숫자 조합 8자 이상 입력해주세요. ';
-    }
-  }else if(target.id == 'password-repeat'){
-    if(target.value===''){
-      span.textContent='비밀번호를 입력해주세요.';
-    }else if(target.value !== inputPw.value){
-      span.textContent = '비밀번호가 다릅니다.';
-    }
+// focusout 되었을 때 
+function handleInputFocusOut({ target }) {
+  switch (target.id) {
+    case 'email':
+      signValidator(spanId, regexId, target);
+      break;
+    case 'password':
+      signValidator(spanPw, regexPw, target);
+      break;
+    case 'password-repeat':
+      checkPassword(spanPwRe, inputPw.value, target);
+      break;
   }
-  span.textContent!==''
-  ? target.classList.add('error-input')
-  : target.classList.remove('error-input');
-}
-
-// 눈 모양 아이콘 띄우고 제거하기 및 버튼 초기화
-function eyeShow({target}){
-  //버튼 초기화
-  signBtn.disabled = false;
-  // 아이콘 띄우고 제거하기
-  if(target.type === 'password'){
-    const targetEye = target.nextElementSibling.nextElementSibling;
-    target.value==''?targetEye.classList.add('none'):targetEye.classList.remove('none'); 
-  }
-  
 }
 
 // 로그인, 회원가입 체크 
 function signCheck() {
-  // 로그인, 회원가입 구분하기
-  if(inputPwRepeat===null){
-    if(inputId.value===user.email&&inputPw.value===user.password){
-      //로그인 성공
-      controller.abort();
-    }else {
-      //로그인 실패
-      signBtn.disabled = true;
-      spanId.textContent = '이메일을 확인해 주세요.';
-      spanPw.textContent = '비밀번호를 확인해 주세요.';
-    }
-  }else if(regexId.test(inputId.value) && inputId.value !== user.email && inputPwRepeat.value !==''&& inputPwRepeat.value === inputPw.value ) {
-    //회원가입 성공
-    controller.abort();
-  }else {
-    // 회원가입 실패
-    signBtn.disabled = true;
-    const spanPwRepeat =  inputPwRepeat.nextElementSibling;
-    spanPw.textContent = inputPw.value ===''?'비밀번호를 입력해주세요.':'';
-    spanId.textContent = inputId.value ==='' ? '이메일을 입력해주세요.': '';
-    if(inputPwRepeat.value !== inputPw.value){
-      spanPwRepeat.textContent = '비밀번호가 다릅니다.';
-    }else if(inputId.value === user.email) {
-      spanId.textContent = '이미 사용 중인 이메일입니다.';
+  if (inputPwRepeat === null) {
+    const url = apiUrl + 'api/sign-in';
+    signTry(url, { email: inputId.value, password: inputPw.value })
+  } else {
+    const values = inputs.map(input => input.value);
+    const checked = values.every(value => value !== '');
+    if (regexId.test(values[0]) && values[1] === values[2] && checked) {
+      const url = apiUrl + 'api/check-email'
+      singupCheck(url, { email: inputId.value, password: inputPw.value });
+    } else {
+      signValidator(spanId, regexId, inputId);
+      signValidator(spanPw, regexPw, inputPw);
+      checkPassword(spanPwRe, inputPw.value, inputPwRepeat);
     }
   }
 }
 
-function enterSignCheck(e){
-  if(e.key ==='Enter'){
-    signCheck();
+// 회원가입 이메일 중복 체크
+async function singupCheck(url, userValue) {
+  try {
+    const postResult = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: userValue.email,
+        password: userValue.password
+      })
+    })
+    if (postResult.ok) {
+      signTry(apiUrl + 'api/sign-up', userValue);
+    } else {
+      setErrorMessage(spanId, inputId, 4);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+// 로그인 회원가입 시도
+async function signTry(url, userValue) {
+  try {
+    const postResult = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // usecredential : true,
+      },
+      body: JSON.stringify({
+        email: userValue.email,
+        password: userValue.password
+      }),
+    });
+    if (postResult.ok) {
+      successSignin();
+    } else {
+      failSignin();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+//sign 성공
+function successSignin() {
+  controller.abort();
+  location.href = '../pages/forder.html'
+}
+
+//로그인 실패
+function failSignin() {
+  setErrorMessage(spanId, inputId, 5);
+  setErrorMessage(spanPw, inputPw, 5);
+}
+
+
+// 눈 모양 아이콘 띄우고 제거하기
+function eyeToggle({ target }) {
+  // 아이콘 띄우고 제거하기
+  if (target.type === 'password') {
+    const targetEye = target.id === 'password' ? eyes[0] : eyes[1];
+    target.value == ''
+      ? targetEye.classList.add('none')
+      : targetEye.classList.remove('none');
   }
 }
 
 // 아이콘 변경
-function eyeOnClick({target}) {
-  const targetPw = target.previousElementSibling.previousElementSibling;
-  if(target.classList.contains('off')){
+function eyeOnClick({ target }) {
+  const targetPw = target.id === 'password-eye-img' ? inputPw : inputPwRepeat;
+  target.classList.toggle('off');
+  if (!target.classList.contains('off')) {
     target.src = '../images/eye-on.svg';
-    target.classList.toggle('off');
     targetPw.type = 'text';
-  }else {
+  } else {
     target.src = '../images/eye-off.svg';
-    target.classList.toggle('off');
     targetPw.type = 'password';
   }
 }
 
-for(let eye of eyes ){
-  eye.addEventListener('click', eyeOnClick, {signal});
-}
+
+//events
+eyes.forEach(eye => {
+  eye.addEventListener('click', eyeOnClick, { signal });
+})
 
 // 모든 input태그에 이벤트 추가하기
-for(let input of inputs){
-  input.addEventListener('focusout', inputFocusOut, {signal});
-  input.addEventListener('change', enterSignCheck, {signal});  
-  input.addEventListener('keyup', eyeShow, {signal});
-}
+inputs.forEach(input => {
+  input.addEventListener('focusout', handleInputFocusOut, { signal });
+  input.addEventListener('keyup', eyeToggle, { signal });
+})
 
 //버튼에 이벤트 추가하기
-signBtn.addEventListener('click', signCheck, {signal});
+signBtn.addEventListener('click', signCheck, { signal });
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(e);
+}, { signal })
