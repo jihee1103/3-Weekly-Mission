ec2b07f8759df4542e84d665f4136a331ea106f3
괴도소연\박소연 <rnlduq157@naver.com>
2023-12-25 07:51:15 +0900

    feat: signin 모듈 스코프 지정 및 로그인 완료 기능
    -저번 주차 때 구현하지 못 했던 로그인 완료 기능 추가
    -validation.js 파일에서 에러메세지 함수를 객체로 묶어 signin.js 파일에서
    또한 수정

--- .github/signin.js
+++ .github/signin.js
@@ -1,22 +1,55 @@
+import { testEmail, testPassword, emailInput, pwInput, isValidEmail, isValidPw } from "./validation.js";
+import { errManager } from "./validation.js";
+
 function emailErrorMessage() {
   const inputEmail = emailInput.value;
 
   if(inputEmail === '') {
-    showEmailError('이메일을 입력해주세요.');
+    errManager.showEmail('이메일을 입력해주세요.');
+  } else if(!isValidEmail(inputEmail)) {
+    errManager.showEmail('올바른 이메일 주소가 아닙니다.')
   } else {
-    hideEmailError();
+    errManager.hideEmail();
   }
 }
 
 function pwErrorMessage() {
-  const inputPW = pwInput.value;
+  const inputPw = pwInput.value;
 
-  if(inputPW === '') {
-    showPasswordError('비밀번호를 입력해주세요.');
+  if(inputPw === '') {
+    errManager.showPassword('비밀번호를 입력해주세요.');
   } else {
-    hidePasswordError();
+    errManager.hidePassword();
   }
 }
 
 emailInput.addEventListener('focusout', emailErrorMessage);
 pwInput.addEventListener('focusout', pwErrorMessage);
+
+const loginForm = document.querySelector('#loginForm');
+
+function loginCheck(e) {
+  const inputEmail = emailInput.value;
+  const inputPw = pwInput.value;
+
+  if(inputEmail === testEmail && inputPw === testPassword) {
+    e.preventDefault();
+    location.href = "folder.html";
+  } else {
+    if(inputEmail !== testEmail) {
+      errManager.showEmail('이메일을 확인해주세요.');
+    } else {
+      errManager.hideEmail();
+    }
+
+    if(inputPw !== testPassword) {
+      errManager.showPassword('비밀번호를 확인해주세요.');
+    } else {
+      errManager.hidePassword();
+    }
+  
+    e.preventDefault();
+  }
+}
+
+loginForm.addEventListener('submit', loginCheck);
