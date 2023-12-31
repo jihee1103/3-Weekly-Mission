cd971244a9526b1c068ec99ee6b79a4143b6864f
Vegatality <vegatality@gmail.com>
2024-01-01 06:31:48 +0900

    feat: 서치바 분리 완성

--- src/pages/shared-page/comp/article/comp/search-bar/SearchBar.jsx
+++ src/pages/shared-page/comp/article/comp/search-bar/SearchBar.jsx
@@ -1,4 +1,13 @@
+import { useInput } from '../../../../../../hooks/useInput';
+import './SearchBar.css';
+
 const SearchBar = () => {
-  return <div>SearchBar</div>;
+  const [_input, onChange, _clearInput] = useInput({ search: '' });
+  return (
+    <div className='searchbar-box'>
+      <i className='searchbar-icon'></i>
+      <input name='search' className='searchbar' type='text' placeholder='링크를 검색해보세요.' onChange={onChange} />
+    </div>
+  );
 };
 export default SearchBar;
