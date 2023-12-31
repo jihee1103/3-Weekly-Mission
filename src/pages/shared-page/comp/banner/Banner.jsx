4d492fac66d921068c24a8bc5948d5487bf85f03
Vegatality <vegatality@gmail.com>
2024-01-01 06:33:11 +0900

    feat: Banner 완성

--- src/pages/shared-page/comp/banner/Banner.jsx
+++ src/pages/shared-page/comp/banner/Banner.jsx
@@ -1,13 +1,19 @@
 import './Banner.css';
 
-const Banner = () => {
+const Banner = ({ userInfo }) => {
   return (
     <div className='hero-banner'>
       <div className='avatar-box'>
-        <img alt='아바타' src={`${process.env.PUBLIC_URL}/images/shared/shared-avatar.svg`} />
-        <span>@코드잇</span>
+        <img
+          className='avatar'
+          alt='아바타'
+          src={
+            !userInfo ? `${process.env.PUBLIC_URL}/images/shared/shared-avatar.svg` : userInfo.owner.profileImageSource
+          }
+        />
+        <span className='folder-owner'>{userInfo ? userInfo.owner.name : '@코드잇'}</span>
       </div>
-      <h1>⭐️ 즐겨찾기</h1>
+      <h1 className='folder-name'>{userInfo ? userInfo.name : '⭐️ 즐겨찾기'}</h1>
     </div>
   );
 };
