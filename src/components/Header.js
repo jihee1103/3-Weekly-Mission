efd83adcf4e5d217a8d64e5166bc8214976952f7
Huni <shkim901112@gmail.com>
2024-01-01 02:32:59 +0900

    ✨ week6

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Profile from "./api/profile";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <a href="/">
+          <img src="/images/logo.png" alt="로고" />
+        </a>
+      </div>
+      <div className="header-content">
+        <Profile />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/api/profile.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+function Profile() {
+  const [profileData, setProfileData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://bootcamp-api.codeit.kr/api/sample/user")
+      .then((response) => response.json())
+      .then((data) => {
+        setProfileData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching profile data:", error);
+      });
+  }, []);
+
+  if (!profileData) {
+    return <button>로그인</button>;
+  }
+
+  return (
+    <div className="profile-items">
+      <img
+        className="profile-image"
+        src={profileData.profileImageSource}
+        alt="Profile"
+      />
+      <p className="profile-email">{profileData.email}</p>
+    </div>
+  );
+}
+
+export default Profile;
