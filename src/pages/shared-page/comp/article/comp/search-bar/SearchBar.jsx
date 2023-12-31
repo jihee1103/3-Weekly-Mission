b2cd825548c6bd6fe2d32fb2e8e700efc78f89e3
Vegatality <vegatality@gmail.com>
2024-01-01 06:44:42 +0900

    fix: deploy 안 돼서 unused varaibles 삭제해서 고침

--- src/pages/shared-page/comp/article/comp/search-bar/SearchBar.jsx
+++ src/pages/shared-page/comp/article/comp/search-bar/SearchBar.jsx
@@ -2,7 +2,7 @@ import { useInput } from '../../../../../../hooks/useInput';
 import './SearchBar.css';
 
 const SearchBar = () => {
-  const [_input, onChange, _clearInput] = useInput({ search: '' });
+  const [, onChange] = useInput({ search: '' });
   return (
     <div className='searchbar-box'>
       <i className='searchbar-icon'></i>
