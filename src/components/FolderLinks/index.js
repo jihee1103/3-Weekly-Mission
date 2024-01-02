98b323a2e1fe542b632cc4caf3db334d7967bf09
haeyong9701 <haeyong9701@gmail.com>
2024-01-02 22:38:19 +0900

    refactor(mentor): handelLinkClick 함수 utils.js로 이동, 불필요한 fragment 제거

--- src/components/FolderLinks/index.js
+++ src/components/FolderLinks/index.js
@@ -1,11 +1,6 @@
 import "./style.css";
-import { formatDate, writtenDate } from "../../utils";
+import { handleLinkClick, formatDate, writtenDate } from "../../utils";
 import noImage from "../../assets/no-image.png";
-
-const handleLinkClick = (event, url) => {
-  event.preventDefault();
-  window.open(url);
-};
 
 const FolderLinks = ({ links, className = "" }) => {
   const classNames = `FolderLinks ${className}`;
@@ -24,18 +19,16 @@ const FolderLinks = ({ links, className = "" }) => {
 const FolderLinkCard = ({ link }) => {
   const { createdAt, url, title, description, imageSource = noImage } = link;
   return (
-    <>
-      <div className="folder-link-box" onClick={(event) => handleLinkClick(event, url)}>
-        <div className="link-image-box">
-          <img className="link-image" src={imageSource} alt={title} />
-        </div>
-        <div className="link-description-box">
-          <div className="written-date">{writtenDate(createdAt)}</div>
-          <div className="description">{description}</div>
-          <div className="format-date">{formatDate(createdAt)}</div>
-        </div>
+    <div className="folder-link-box" onClick={(event) => handleLinkClick(event, url)}>
+      <div className="link-image-box">
+        <img className="link-image" src={imageSource} alt={title} />
       </div>
-    </>
+      <div className="link-description-box">
+        <div className="written-date">{writtenDate(createdAt)}</div>
+        <div className="description">{description}</div>
+        <div className="format-date">{formatDate(createdAt)}</div>
+      </div>
+    </div>
   );
 };
 
