57a324dc546ed0ce2eea6fb019e1347773d45ef5
Jo hyeonjin <guswlso2@naver.com>
2024-01-02 16:23:49 +0900

    api함수 베이스 주소 상수화

--- src/api.js
+++ src/api.js
@@ -1,27 +1,22 @@
+const API_BASE_URL = "https://bootcamp-api.codeit.kr";
 export async function getUserId() {
-  const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/user",
-    {
-      method: "GET",
-      headers: {
-        accept: "*/*",
-      },
-    }
-  );
+  const response = await fetch(`${API_BASE_URL}/api/sample/user`, {
+    method: "GET",
+    headers: {
+      accept: "*/*",
+    },
+  });
 
   return await response.json();
 }
 
 export async function getUserFolder() {
-  const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/folder",
-    {
-      method: "GET",
-      headers: {
-        accept: "*/*",
-      },
-    }
-  );
+  const response = await fetch(`${API_BASE_URL}/api/sample/folder`, {
+    method: "GET",
+    headers: {
+      accept: "*/*",
+    },
+  });
 
   return await response.json();
 }
