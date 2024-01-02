bcd800e7053eae7a19d85003bf5270dcaeaa79a9
haeyong9701 <haeyong9701@gmail.com>
2024-01-02 23:37:35 +0900

    refactor(mentor): 상수명 UPPER_SNAKE_CASE로 변경

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
-const api = "https://bootcamp-api.codeit.kr/api";
+const API_BASE_URL = "https://bootcamp-api.codeit.kr/api";
 
 export const getSampleUser = async () => {
-  const response = await fetch(`${api}/sample/user`);
+  const response = await fetch(`${API_BASE_URL}/sample/user`);
   if (!response.ok) {
     throw new Error("데이터를 불러오는데 실패했습니다");
   }
@@ -10,7 +10,7 @@ export const getSampleUser = async () => {
 };
 
 export const getSampleFolder = async () => {
-  const response = await fetch(`${api}/sample/folder`);
+  const response = await fetch(`${API_BASE_URL}/sample/folder`);
   if (!response.ok) {
     throw new Error("데이터를 불러오는데 실패했습니다");
   }
