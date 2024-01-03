441bd6a58078e836e6e04702abb77f7d26f52c5e
jinyoung1018 <jywsb1816@gmail.com>
2024-01-03 23:29:00 +0900

    주석 제거

--- src/api/api.js
+++ src/api/api.js
@@ -10,26 +10,4 @@ const getFolderData = async () => {
   return result;
 };
 
-// const getUserData = async () => {
-//   try {
-//     const res = await fetch("https://bootcamp-api.codeit.kr/api/sample/user");
-//     const result = await res.json();
-//     return result;
-//   } catch (error) {
-//     console.error("Error fetching user data:", error);
-//     throw error; // Re-throw the error to propagate it to the calling code
-//   }
-// };
-
-// const getFolderData = async () => {
-//   try {
-//     const res = await fetch("https://bootcamp-api.codeit.kr/api/sample/folder");
-//     const result = await res.json();
-//     return result;
-//   } catch (error) {
-//     console.error("Error fetching folder data:", error);
-//     throw error; // Re-throw the error to propagate it to the calling code
-//   }
-// };
-
 export { getUserData, getFolderData };
