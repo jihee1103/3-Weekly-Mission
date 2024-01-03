3d6bdbab5722febf7a7d62fdeadebfe9e6348a41
Dev-Duke-Seo <dev.duke.seo@gmail.com>
2024-01-03 12:17:51 +0900

    Feat : folder list 기능구현
    1. Api response-> folder list를 grid로 렌더링.
    2. style 추가.

--- src/pages/favorites/Favorites.js
+++ src/pages/favorites/Favorites.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../../styles/pages/favorites.css';
 import Top from "./Top";
 import Contents from "./Contents";
+import {getFolderList} from "../../utils/api";
 
 function Favorites() {
+  const [data, setData] = React.useState(null);
+
+  const handleLoadFolderList = async () => {
+    let result;
+    try {
+      result = await getFolderList();
+      setData(result.folder);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    handleLoadFolderList();
+  }, []);
+
   return (
       <div className="favorites">
-        <Top/>
-        <Contents/>
+        {data && <Top data={data}/>}
+        {data && <Contents data={data.links}/>}
       </div>);
 }
 
