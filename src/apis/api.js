89f3a3f0ab2bf91250980fee72633271f3902669
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-03 19:22:05 +0900

    chore: api 공통함수 이름 변경

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,4 +1,4 @@
-export async function bodyResponseJson(url) {
+export async function fetchJson(url) {
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("데이터를 불러오는데 실패했습니다.");
@@ -10,12 +10,12 @@ export async function bodyResponseJson(url) {
 
 export async function getUser() {
   const url = "https://bootcamp-api.codeit.kr/api/sample/user";
-  const response = await bodyResponseJson(url);
+  const response = await fetchJson(url);
   return response;
 }
 
 export async function getShredCardList() {
   const url = "https://bootcamp-api.codeit.kr/api/sample/folder";
-  const response = await bodyResponseJson(url);
+  const response = await fetchJson(url);
   return response;
 }
