19ac654dc4035c28739faf54b9d9fc23118643fa
siyeol97 <leesi2830@naver.com>
2024-01-04 00:19:09 +0900

    fix: calculateTime 함수 외부 파일에 분리, 리팩터링

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import "../css/Card.css";
 import defaultImage from "../asset/default-image.svg";
 import { useEffect, useState } from "react";
+import calculateTime from "../utils/calculateTime";
 
 export default function Card({ link }) {
   const [formattedCreatedAt, setFormattedCreatedAt] = useState("");
@@ -19,35 +20,12 @@ export default function Card({ link }) {
   };
 
   useEffect(() => {
-    const calculateTime = (createdAt) => {
-      const createdTime = new Date(createdAt);
-      const now = new Date();
+    const formattedElapsedTime = calculateTime(link.createdAt);
 
-      const timeDifferenceSeconds = Math.floor((now.getTime() - createdTime.getTime()) / 1000);
-      const timeDifferenceMinutes = Math.floor(timeDifferenceSeconds / 60);
-      const timeDifferenceHours = Math.floor(timeDifferenceMinutes / 60);
-      const timeDifferenceDays = Math.floor(timeDifferenceHours / 24);
-      const timeDifferenceMonths = Math.floor(timeDifferenceDays / 31);
-      const timeDifferenceYears = Math.floor(timeDifferenceMonths / 12);
-
-      // 경과 시간 설정
-      if (timeDifferenceMinutes < 2) setElapsedTime(`1 minute ago`);
-      else if (timeDifferenceMinutes <= 59) setElapsedTime(`${timeDifferenceMinutes} minutes ago`);
-      else if (timeDifferenceMinutes >= 60 && timeDifferenceMinutes < 120)
-        setElapsedTime(`1 hour ago`);
-      else if (timeDifferenceHours <= 23) setElapsedTime(`${timeDifferenceHours} hours ago`);
-      else if (timeDifferenceHours >= 24 && timeDifferenceDays < 2) setElapsedTime(`1 day ago`);
-      else if (timeDifferenceDays <= 30) setElapsedTime(`${timeDifferenceDays} days ago`);
-      else if (timeDifferenceDays >= 31 && timeDifferenceMonths < 2) setElapsedTime(`1 month ago`);
-      else if (timeDifferenceMonths <= 11) setElapsedTime(`${timeDifferenceMonths} months ago`);
-      else if (timeDifferenceMonths >= 12 && timeDifferenceYears < 2) setElapsedTime(`1 year ago`);
-      else setElapsedTime(`${timeDifferenceYears} years ago`);
-    };
-
+    setElapsedTime(formattedElapsedTime);
     setLinkUrl(link.url);
     setCardImgUrl(link.imageSource);
     updateFormattedCreatedAt(link.createdAt);
-    calculateTime(link.createdAt);
   }, [link]);
 
   const handleCardClick = (link) => {
