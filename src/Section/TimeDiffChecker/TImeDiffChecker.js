305316ad0cedb2475a40838dc8b5a264427299e6
SongSang <941227_@naver.com>
2024-01-04 18:34:17 +0900

    refactor: TimeDiff 함수 상수 선언 수정 소문자->대문자

--- src/Section/TimeDiffChecker/TImeDiffChecker.js
+++ src/Section/TimeDiffChecker/TImeDiffChecker.js
@@ -1,27 +1,27 @@
 function getTimeDifference(time) {
-  const second = 1;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const mon = day * 30;
+  const SECOND = 1;
+  const MINUTE = SECOND * 60;
+  const HOUR = MINUTE * 60;
+  const DAY = HOUR * 24;
+  const MONTH = DAY * 30;
 
-  if (time < minute * 2) {
+  if (time < MINUTE * 2) {
     return `1 minute ago`;
-  } else if (time < minute * 59) {
+  } else if (time < MINUTE * 59) {
     return `${Math.trunc(time / 60)} minutes ago`;
-  } else if (time > minute * 60 && time < day) {
+  } else if (time > MINUTE * 60 && time < DAY) {
     return `${Math.trunc(time / 60 / 60)} hours ago`;
-  } else if (time > day && time < day * 2) {
+  } else if (time > DAY && time < DAY * 2) {
     return `1 day ago`;
-  } else if (time < day * 30) {
+  } else if (time < DAY * 30) {
     return `${Math.trunc(time / 60 / 60 / 24)} days ago`;
-  } else if (time >= day * 30 && time < day * 60) {
+  } else if (time >= DAY * 30 && time < DAY * 60) {
     return `1 month ago`;
-  } else if (time < mon * 12) {
+  } else if (time < MONTH * 12) {
     return `${Math.trunc(time / 60 / 60 / 24 / 30)} months ago`;
-  } else if (time >= mon * 12 && time < mon * 24) {
+  } else if (time >= MONTH * 12 && time < MONTH * 24) {
     return `1 year ago`;
-  } else if (time >= mon * 24) {
+  } else if (time >= MONTH * 24) {
     return `${Math.trunc(time / 60 / 60 / 24 / 30 / 12)} years ago`;
   }
 }
