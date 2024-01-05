a319ec8e2471e2c0e63a6d2b61126ce15f34df4e
giho <giho7427@daum.net>
2024-01-05 21:44:39 +0900

    [feat] 폴더버튼의 상태가 전체일때 폴더툴바가 안보이게 함

--- src/components/CardList/Card/Card.jsx
+++ src/components/CardList/Card/Card.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const AlertNotStoredLink = styled.div`
@@ -15,7 +17,72 @@ const AlertNotStoredLink = styled.div`
   line-height: 24px; /* 150% */
 `;
 
+const KebabBtn = styled.button`
+  width: 21px;
+  height: 17px;
+  img {
+    width: 100%;
+    height: 100%;
+  }
+
+  &:hover {
+    border-radius: 2px;
+    background-color: #313131;
+    color: white;
+  }
+`;
+
+const KebabMenu = styled.div`
+  display: flex;
+  width: 100px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 2px;
+  position: absolute;
+  right: -80px;
+  top: 16px;
+`;
+
+const KebabMenuDeleteBtn = styled.button`
+  display: flex;
+  padding: 7px 12px;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
+  color: var(--gray-light-gray-100, #333236);
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  background: var(--gray-light-gray-00, #fff);
+  box-shadow: 0px 2px 8px 0px rgba(51, 50, 54, 0.1);
+`;
+
+const KebabMenuAddFolderBtn = styled.button`
+  display: flex;
+  padding: 7px 12px;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 10px;
+  align-self: stretch;
+  background: var(--Linkbrary-gray10, #e7effb);
+  color: var(--Linkbrary-primary-color, #6d6afe);
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
 const Card = ({ cardData }) => {
+  const [kebabToggle, setKebabToggle] = useState(false);
+
+  const handleKebabToggle = () => {
+    setKebabToggle(!kebabToggle);
+  };
+
   const calculatePassedTime = (createdAt) => {
     const nowTime = Date.now();
     const uploadedTime = new Date(createdAt).getTime();
@@ -57,24 +124,51 @@ const Card = ({ cardData }) => {
       {cardData.length !== 0 ? (
         cardData.map((link) => {
           return (
-            <a href={link.url} target="_blank" className="link" key={link.id} rel="noreferrer">
+            <Link to={link.url} target="_blank" className="link" key={link.id} rel="noreferrer">
               <div>
                 <div className="link__img">
                   {link.image_source ?? link.imageSource ? (
-                    <img src={link.image_source ?? link.imageSource} alt="카드 이미지" />
+                    <img className="link__img--card" src={link.image_source ?? link.imageSource} alt="카드 이미지" />
                   ) : (
-                    <img src={`${process.env.PUBLIC_URL}/images/no_image_source.svg`} alt="이미지가 없음" />
+                    <img
+                      className="link__img--card"
+                      src={`${process.env.PUBLIC_URL}/images/no_image_source.svg`}
+                      alt="이미지가 없음"
+                    />
                   )}
+                  <img
+                    src={`${process.env.PUBLIC_URL}/images/star.svg`}
+                    alt="즐겨찾기 별 이미지"
+                    className="link__img--star"
+                  />
                 </div>
                 <div className="link__content">
                   <div className="link__content--time-passed">
                     {calculatePassedTime(link.created_at ?? link.createdAt)}
+                    <KebabBtn
+                      onClick={(e) => {
+                        e.preventDefault();
+                        handleKebabToggle();
+                      }}
+                    >
+                      <img
+                        className="link__content--More-Btn"
+                        src={`${process.env.PUBLIC_URL}/images/kebab.svg`}
+                        alt="더보기 케밥 버튼"
+                      />
+                    </KebabBtn>
+                    {kebabToggle ? (
+                      <KebabMenu>
+                        <KebabMenuDeleteBtn>삭제하기</KebabMenuDeleteBtn>
+                        <KebabMenuAddFolderBtn>폴더에 추가</KebabMenuAddFolderBtn>
+                      </KebabMenu>
+                    ) : null}
                   </div>
                   <div className="link__content--description">{link.description}</div>
                   <div className="link__content--createdAt">{getFormattedDate(link.created_at ?? link.createdAt)}</div>
                 </div>
               </div>
-            </a>
+            </Link>
           );
         })
       ) : (
