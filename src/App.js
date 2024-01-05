bd61d0d9e37d4351546c52e4c0143261bd7f20ea
jihee <sweet9811@gmail.com>
2024-01-06 00:58:11 +0900

    feature: Main컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
 import Profile from "./components/Profile";
 import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 const App = function () {
   return (
     <>
       <Header />
       <Profile />
+      <Main />
       <Footer />
     </>
   );
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import "./Main.css";
+
+const Main = function () {
+  return (
+    <div className="Main">
+      <div className="content-box">
+        <input className="search-bar" placeholder="링크를 검색해 보세요." />
+        <div>cards</div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
