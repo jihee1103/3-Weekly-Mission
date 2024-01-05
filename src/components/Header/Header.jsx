10953a7ef729b923551a38ac84c7d64047f32360
giho <giho7427@daum.net>
2024-01-05 22:12:52 +0900

    [fix] Header 컴포넌트 styled Component로 변경

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,58 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import HeaderProfile from './HeaderProfile/HeaderProfile';
+
+const HeaderWrap = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 32px 200px;
+  background: var(--gray5);
+  position: fixed;
+  z-index: 9999;
+
+  @media (max-width: 1123px) and (min-width: 768px) {
+    padding: 20px 32px;
+  }
+
+  @media (max-width: 767px) {
+    padding: 13px 32px;
+  }
+`;
+
+const HeaderContainer = styled.div`
+  max-width: 1920px;
+  display: flex;
+  justify-content: space-between;
+  margin: 0 auto;
+
+  @media (max-width: 1123px) and (min-width: 768px) {
+    width: 800px;
+  }
+
+  @media (max-width: 864px) {
+    width: auto;
+  }
+
+  @media (max-width: 767px) {
+    width: 100%;
+  }
+`;
+
+const LogoImg = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
 const Header = ({ login, userData }) => {
   return (
-    <div className="header">
-      <div className="header--container">
-        <a href="/" className="header__img">
-          <img src={`${process.env.PUBLIC_URL}/images/logo.svg`} alt="로고 이미지" />
-        </a>
-        {login ? (
-          <div className="header__profile">
-            <img src={userData.image_source} alt="프로필 이미지" />
-            <div>{userData.email}</div>
-          </div>
-        ) : (
-          <button type="button" className="header__btn-login">
-            <a href="/signin">로그인</a>
-          </button>
-        )}
-      </div>
-    </div>
+    <HeaderWrap>
+      <HeaderContainer>
+        <Link to="/" className="header__img">
+          <LogoImg src={`${process.env.PUBLIC_URL}/images/logo.svg`} alt="로고 이미지" />
+        </Link>
+        <HeaderProfile login={login} userData={userData} />
+      </HeaderContainer>
+    </HeaderWrap>
   );
 };
 
---
+++ src/components/Header/HeaderProfile/HeaderProfile.jsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const HeaderProfileContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 28px;
+    height: 28px;
+    border-radius: 50%;
+    margin-right: 6px;
+  }
+  div {
+    color: var(--Linkbrary-gray100, #373740);
+    font-family: Pretendard;
+    font-size: 14px;
+  }
+`;
+
+const HeaderLoginInBtn = styled.button`
+  width: 128px;
+  border-radius: 8px;
+  padding: 16px 20px;
+  color: var(--Grey-Light, #f5f5f5);
+  font-family: Pretendard;
+  font-size: 18px;
+  font-weight: 600;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  background: var(--gra-purpleblue-to-skyblue, linear-gradient(91deg, #6d6afe 0.12%, #6ae3fe 101.84%));
+`;
+
+const HeaderProfile = ({ login, userData }) => {
+  return (
+    <div>
+      {login ? (
+        <HeaderProfileContainer>
+          <img src={userData.image_source} alt="프로필 이미지" />
+          <div>{userData.email}</div>
+        </HeaderProfileContainer>
+      ) : (
+        <HeaderLoginInBtn type="button">
+          <Link to="/signin">로그인</Link>
+        </HeaderLoginInBtn>
+      )}
+    </div>
+  );
+};
+
+export default HeaderProfile;
