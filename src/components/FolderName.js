3466e5e0a422f48647f051dfda5886363a92c982
Jo hyeonjin <guswlso2@naver.com>
2024-01-05 15:44:33 +0900

    전체 폴더 선택시 폴더편집박스가 안보이게 수정

--- src/components/FolderName.js
+++ src/components/FolderName.js
@@ -5,20 +5,24 @@ export default function FolderName({ selectedFolder }) {
       <div className="folder-name">
         {selectedFolder ? selectedFolder.name : "전체"}
       </div>
-      <div className="folder-edit-box">
-        <div>
-          <img src="/imgs/share.png" alt="공유하기" />
-          <span>공유</span>
+      {selectedFolder ? (
+        <div className="folder-edit-box">
+          <div>
+            <img src="/imgs/share.png" alt="공유하기" />
+            <span>공유</span>
+          </div>
+          <div>
+            <img src="/imgs/pen.png" alt="이름변경하기" />
+            <span>이름변경</span>
+          </div>
+          <div>
+            <img src="/imgs/delete.png" alt="삭제하기" />
+            <span>삭제</span>
+          </div>
         </div>
-        <div>
-          <img src="/imgs/pen.png" alt="이름변경하기" />
-          <span>이름변경</span>
-        </div>
-        <div>
-          <img src="/imgs/delete.png" alt="삭제하기" />
-          <span>삭제</span>
-        </div>
-      </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
