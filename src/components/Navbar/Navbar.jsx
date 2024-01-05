d462f7bea3621dc7bfa33c625b9f003fc0fd6a14
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-05 20:46:44 +0900

    chore: Navbar에 있는 a태그 Link태그로 변경

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,24 @@
 import "./Navbar.css";
 import logoImg from "../../assets/images/logo.svg";
 import defaultProfileImg from "../../assets/images/shared-myprofile.svg";
+import { Link } from "react-router-dom";
 
 export default function Navbar({ user }) {
   return (
     <nav className="navbar">
       <div className="navbar-content">
-        <a href="./index.html">
+        <Link to={"/"}>
           <img src={logoImg} className="logo" alt="홈으로 이동하는 로고" />
-        </a>
+        </Link>
 
         {user ? (
           <NavProfile user={user} />
         ) : (
-          <a href="week5/auth/signin.html">
+          <Link to={"/"}>
             <button type="button" className="login-btn">
               로그인
             </button>
-          </a>
+          </Link>
         )}
       </div>
     </nav>
