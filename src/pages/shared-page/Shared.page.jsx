e1cc28f77954e4a6f965852de1ee808acdbc09d7
Vegatality <vegatality@gmail.com>
2024-01-08 21:26:16 +0900

    Feat: shared 페이지에도 문서 타이틀 추가

--- src/pages/shared-page/Shared.page.jsx
+++ src/pages/shared-page/Shared.page.jsx
@@ -1,15 +1,17 @@
-import Header from '@layout/header&footer/comp/header/Header';
-import Footer from '@layout/header&footer/comp/footer/Footer';
+import Header from './comp/header/Header';
+import Footer from './comp/footer/Footer';
 import Article from './comp/article/Article';
 import Banner from './comp/banner/Banner';
 import { useGetUserFolders } from './hooks/useGetUserFolders';
 import './Shared.css';
+import DocumentTitle from '@layout/document-title/DocumentTitle';
 
 const Shared = () => {
   const { links, userInfo } = useGetUserFolders();
 
   return (
     <>
+      <DocumentTitle title='Shared' />
       <Header />
       <Banner userInfo={userInfo} />
       <Article links={links} />
