74185f5e56e241c6030057c22ac22823c591bdd2
jinyoung1018 <jywsb1816@gmail.com>
2024-01-10 21:57:31 +0900

    함수 export 방식 변경

--- src/api/api.js
+++ src/api/api.js
@@ -1,39 +1,31 @@
 const API_BASE_URL = "https://bootcamp-api.codeit.kr";
 
-const getUserData = async () => {
+export const getUserData = async () => {
   const res = await fetch(`${API_BASE_URL}/api/sample/user`);
   const result = await res.json();
   return result;
 };
 
-const getFolderData = async () => {
+export const getFolderData = async () => {
   const res = await fetch(`${API_BASE_URL}/api/sample/folder`);
   const result = await res.json();
   return result;
 };
 
-const getFolderList = async () => {
+export const getFolderList = async () => {
   const res = await fetch(`${API_BASE_URL}/api/users/1/folders`);
   const result = await res.json();
   return result;
 };
 
-const getAllCardData = async () => {
+export const getAllCardData = async () => {
   const res = await fetch(`${API_BASE_URL}/api/users/1/links`);
   const result = await res.json();
   return result;
 };
 
-const getCardDataById = async (id) => {
+export const getCardDataById = async (id) => {
   const res = await fetch(`${API_BASE_URL}/api/users/1/links?folderId=${id}`);
   const result = await res.json();
   return result;
-};
-
-export {
-  getUserData,
-  getFolderData,
-  getFolderList,
-  getAllCardData,
-  getCardDataById,
 };
