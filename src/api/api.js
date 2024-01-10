47701993897b477ebeb979fb60bb1d19e8a5bf1c
jinyoung1018 <jywsb1816@gmail.com>
2024-01-10 22:01:26 +0900

    불필요한 선언 제거

--- src/api/api.js
+++ src/api/api.js
@@ -2,30 +2,25 @@ const API_BASE_URL = "https://bootcamp-api.codeit.kr";
 
 export const getUserData = async () => {
   const res = await fetch(`${API_BASE_URL}/api/sample/user`);
-  const result = await res.json();
-  return result;
+  return res.json();
 };
 
 export const getFolderData = async () => {
   const res = await fetch(`${API_BASE_URL}/api/sample/folder`);
-  const result = await res.json();
-  return result;
+  return res.json();
 };
 
 export const getFolderList = async () => {
   const res = await fetch(`${API_BASE_URL}/api/users/1/folders`);
-  const result = await res.json();
-  return result;
+  return res.json();
 };
 
 export const getAllCardData = async () => {
   const res = await fetch(`${API_BASE_URL}/api/users/1/links`);
-  const result = await res.json();
-  return result;
+  return res.json();
 };
 
 export const getCardDataById = async (id) => {
   const res = await fetch(`${API_BASE_URL}/api/users/1/links?folderId=${id}`);
-  const result = await res.json();
-  return result;
+  return res.json();
 };
