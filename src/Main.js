61248b79cf9d72ac5b90cb7929706d8022af347c
haeyong9701 <haeyong9701@gmail.com>
2024-01-11 22:18:28 +0900

    move: App 폴더 생성 후 App 컴포넌트 파일들 이동

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import App from "./components/App";
 import SharedPage from "./pages/SharedPage";
 import FolderPage from "./pages/FolderPage";
 import "./styles/global.css";
