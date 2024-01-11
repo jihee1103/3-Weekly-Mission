61a1cfea5af842b4da40568251167c17f190b678
haeyong9701 <haeyong9701@gmail.com>
2024-01-12 02:36:47 +0900

    feat: AddLinkBar 컴포넌트 생성

--- src/pages/FolderPage/index.js
+++ src/pages/FolderPage/index.js
@@ -1,12 +1,26 @@
-import Footer from "../../components/Footer";
-import Nav from "../../components/Nav";
 import "./style.css";
+import search from "../../assets/search.svg";
+import Nav from "../../components/Nav";
+import Footer from "../../components/Footer";
+import AddLinkBar from "../../components/AddLinkBar";
 
 const FolderPage = ({ user, folderObj }) => {
+  const { folder = {} } = folderObj;
+  const { owner = {}, links = [] } = folder;
+
   return (
     <>
       <Nav user={user} />
-      폴더 페이지
+      <main>
+        <AddLinkBar />
+        <section className="folder-links">
+          <div className="link-search-bar">
+            <img className="link-search-icon" src={search} alt="검색 아이콘" />
+            <input className="link-search" placeholder="링크를 검색해 보세요." />
+          </div>
+          {/* <SharedLinks className="SharedPage-folder-links" links={links} /> */}
+        </section>
+      </main>
       <Footer />
     </>
   );
