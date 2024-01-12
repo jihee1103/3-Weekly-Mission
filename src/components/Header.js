0ad906f430b1497824f95e5a65c1c22aba356eff
jihee <sweet9811@gmail.com>
2024-01-12 18:06:43 +0900

    fix: 로그인이 되어 있으면 프로필이 보이도록 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,32 @@
+import getUser from "../api/getUser";
 import "./Header.css";
+import { useEffect, useState } from "react";
 
 const Header = function () {
+  const [profileData, setProfileData] = useState([]);
+
+  useEffect(() => {
+    const data = async () => {
+      const result = await getUser();
+      setProfileData(result);
+    };
+
+    data();
+  }, []);
+
+  const logInButton = <button className="cta cta-short">로그인</button>;
+  const userProfile = (
+    <div className="userProfile">
+      <img className="profileImg" src={profileData.profileImageSource} />
+      <p>{profileData.email}</p>
+    </div>
+  );
+
   return (
     <div className="header">
       <nav>
         <img src="/img/logo.svg" alt="Linkbrary 로고" />
-        <button className="cta cta-short">로그인</button>
+        {profileData === "[]" ? logInButton : userProfile}
       </nav>
     </div>
   );
