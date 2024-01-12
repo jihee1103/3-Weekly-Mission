c2123554d1c2041f002c1893241fb920b97c0757
jihee <sweet9811@gmail.com>
2024-01-12 09:44:12 +0900

    fix: 카드 이미지 hover움직임 수정 및 no-image추가

--- src/card/CardImage.js
+++ src/card/CardImage.js
@@ -1,12 +1,10 @@
 import "./CardImage.css";
 
-const CardImage = function ({ isHovered, imageSource }) {
-  return (
-    <img
-      className={isHovered ? "CardImage zoomedCardImage" : "CardImage"}
-      src={imageSource}
-    />
-  );
+const CardImage = function ({ isHovered, imageSource, title }) {
+  const DEFAULT_IMAGE = "/img/no_image.png";
+  const className = isHovered ? "CardImage zoomedCardImage" : "CardImage";
+
+  return <div style={{ backgroundImage: `url(${imageSource ?? DEFAULT_IMAGE})` }} alt={title} className={className} />;
 };
 
 export default CardImage;
