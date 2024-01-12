853f31c3aa2493ee3c0c73cca2048bd4588dea2b
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-12 19:14:23 +0900

    refator: CtaButton.jsx styledComponents로 수정

--- src/components/CtaButton/CtaButton.jsx
+++ src/components/CtaButton/CtaButton.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-import styled from "./CtaButton.module.css";
+import styled from "styled-components";
 
-export default function CtaButton({ children }) {
+export default function CtaButton({ CTAButtonStyle = CTAButton, children }) {
   return (
-    <button className={styled.button}>
-      <span>{children}</span>
-    </button>
+    <CTAButtonStyle>
+      <ButtonText>{children}</ButtonText>
+    </CTAButtonStyle>
   );
 }
+
+export const CTAButton = styled.button`
+  text-align: center;
+  padding: 10px 16px;
+  border-radius: 8px;
+  border: none;
+  background: var(
+    --gra-purpleblue-to-skyblue,
+    linear-gradient(91deg, #6d6afe 0.12%, #6ae3fe 101.84%)
+  );
+`;
+
+const ButtonText = styled.span`
+  display: inline-block;
+  font-size: 14px;
+  font-weight: 600;
+  width: 49px;
+`;
--- src/components/Modal/ModalAdd.jsx
+++ src/components/Modal/ModalAdd.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import {
-  ModalButton,
-  ModalInput,
-  ModalTitle,
-  ModalWrapper,
-} from "./Modal.styled";
+import { ModalInput, ModalTitle, ModalWrapper } from "./Modal.styled";
+import styled from "styled-components";
+import CtaButton, { CTAButton } from "../CtaButton/CtaButton";
 
 export default function ModalAdd() {
   return (
     <ModalWrapper>
       <ModalTitle>폴더 추가</ModalTitle>
       <ModalInput placeholder={"내용 입력"} />
-      <ModalButton>추가하기</ModalButton>
+      <CtaButton CTAButtonStyle={ModalCtaButton}>추가하기</CtaButton>
     </ModalWrapper>
   );
 }
+
+const ModalCtaButton = styled(CTAButton)`
+  padding: 16px 20px;
+  width: 280px;
+`;
