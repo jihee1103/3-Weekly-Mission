b679a2ba955941ed6221dce3e1c032633d6cb5e7
jihee <sweet9811@gmail.com>
2024-01-12 17:31:24 +0900

    fix: profile데이터를 fetch로 가져오도록 수정

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,36 @@
 import "./Profile.css";
+import getUser from "../api/getUser";
+import getFolder from "../api/getFolder";
+import { useState, useEffect } from "react";
 
 const Profile = function () {
+  const [folderName, setFolderName] = useState([]);
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    const getFolderData = async () => {
+      const result = await getFolder();
+      setFolderName(result.folder.name);
+    };
+
+    getFolderData();
+
+    const getUserData = async () => {
+      const result = await getUser();
+      setUserData(result);
+    };
+
+    getUserData();
+  }, []);
+
   return (
     <div className="Profile">
       <div className="box">
         <div className="personal">
-          <img src="/img/codeit_logo.svg" alt="profile사진" />
-          <span>@코드잇</span>
+          <img src={userData.profileImageSource} alt="profile사진" />
+          <span>@{userData.name}</span>
         </div>
-        <span className="bookmark">즐겨찾기</span>
+        <span className="bookmark">{folderName}</span>
       </div>
     </div>
   );
