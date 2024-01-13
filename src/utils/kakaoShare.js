92f9e48de29c7dae42d0277ef125edc61d53eaae
SiWooJinSeok <59861974+SiWooJinSeok@users.noreply.github.com>
2024-01-13 22:18:16 +0900

    test: 카카오링크 공유시의 이미지 변경

--- src/utils/kakaoShare.js
+++ src/utils/kakaoShare.js
@@ -10,7 +10,8 @@ export const shareKakao = (url, folderName) => {
     content: {
       title: "Linkbrary",
       description: `${folderName} 페이지`,
-      imageUrl: imageData.defaultImg,
+      imageUrl:
+        "https://visitbusan.net/uploadImgs/files/cntnts/20211130150754165_wufrotr",
       link: {
         // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
         mobileWebUrl: url,
