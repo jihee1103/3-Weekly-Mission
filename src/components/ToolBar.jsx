60265fc432082f650f0bfa4a3fb497da449b8917
jihee <sweet9811@gmail.com>
2024-01-14 12:49:49 +0900

    fix: alt속성 추가

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -6,15 +6,15 @@ const ToolBar = function () {
       <div className="text">유용한 글</div>
       <div className="tools">
         <div className="tool">
-          <img src="/img/share_icon.svg" />
+          <img src="/img/share_icon.svg" alt="공유" />
           공유
         </div>
         <div className="tool">
-          <img src="/img/pen_icon.svg" />
+          <img src="/img/pen_icon.svg" alt="이름변경" />
           이름 변경
         </div>
         <div className="tool">
-          <img src="/img/delete_icon.svg" />
+          <img src="/img/delete_icon.svg" alt="삭제" />
           삭제
         </div>
       </div>
--- src/components/addLink/AddLink.jsx
+++ src/components/addLink/AddLink.jsx
@@ -6,7 +6,7 @@ const AddLink = function () {
       <div className="AddLink">
         <div className="InputImage">
           <input className="AddLinkInput" placeholder="링크를 추가해 보세요" />
-          <img className="AddLinkImage" src="/img/link_icon.svg" />
+          <img className="AddLinkImage" src="/img/link_icon.svg" alt="검색" />
         </div>
         <button className="AddLinkCta">추가하기</button>
       </div>
