568424f772904046e8a30f3d1aa8a27dd23f1160
clyde <clydehan0401@gmail.com>
2024-01-15 07:47:12 +0900

    feat: Add and Fix css style

--- src/components/FolderSectionBar/folder-section-bar.jsx
+++ src/components/FolderSectionBar/folder-section-bar.jsx
@@ -2,10 +2,10 @@ import "./folder-section-bar-style.css";
 
 export function FolderSectionBar({ name, onClick }) {
   return (
-    <>
+    <div className="filter-section-container">
       <button className="filter-button" onClick={onClick}>
         {name}
       </button>
-    </>
+    </div>
   );
 }
