4fcef7e33a8bb27a6791db00af05eb6f1fd8964e
clyde <clydehan0401@gmail.com>
2024-02-11 12:32:32 +0900

    feat: folder 페이지 설계

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import FolderPage from "./page/FolderPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route path="/folder" element={<FolderPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/page/FolderPage.js
@@ -0,0 +1,21 @@
+const FolderPage = () => {
+  return (
+    <>
+      <header>
+        <nav>로고, 프로필</nav>
+        <div>링크 추가 인풋</div>
+      </header>
+      <main>
+        <section>링크 검색 인풋</section>
+        <section>폴더이름 버튼 섹션</section>
+        <section>
+          <nav>폴더제목, 기능아이콘</nav>
+          <article>컨텐츠 리스트</article>
+        </section>
+      </main>
+      <footer>푸터</footer>
+    </>
+  );
+};
+
+export default FolderPage;
