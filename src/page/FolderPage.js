6a2813f2d8741741dad5521ffb641efeeb5f4e76
clyde <clydehan0401@gmail.com>
2024-02-11 12:35:41 +0900

    fix: 시맨틱 태그 수정

--- src/page/FolderPage.js
+++ src/page/FolderPage.js
@@ -7,9 +7,12 @@ const FolderPage = () => {
       </header>
       <main>
         <section>링크 검색 인풋</section>
-        <section>폴더이름 버튼 섹션</section>
+        <nav>폴더이름 버튼 섹션</nav>
         <section>
-          <nav>폴더제목, 기능아이콘</nav>
+          <div>
+            <h1>폴더 제목</h1>
+            <div>기능아이콘</div>
+          </div>
           <article>컨텐츠 리스트</article>
         </section>
       </main>
