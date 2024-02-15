631a3926ce798871384a8c1c5a83bffdf49db880
Vegatality <vegatality@gmail.com>
2024-02-15 16:14:07 +0900

    refactor: 필요없는 로직 제거

--- src/utils/search/filterMatchedDatas.ts
+++ src/utils/search/filterMatchedDatas.ts
@@ -3,16 +3,11 @@ export const filterMatchedDatas = <T extends NonNullable<unknown>, U extends key
   matchingKeyword: string | undefined,
   matchingField: Array<U>,
 ): boolean => {
-  if (matchingKeyword === '') return true;
+  if (!matchingKeyword) return true;
 
   if (!matchingTarget) return false;
 
-  for (let i = 0; i < matchingField.length; i++) {
-    if (!matchingTarget[matchingField[i]] || typeof matchingTarget[matchingField[i]] !== 'string') return false;
-  }
-
-  // ''은 모든 문자열에 포함되므로, ''일 경우는 true를 반환한다.(즉, 검색어가 없을 경우는 모든 데이터를 반환한다.)
-  const lowerCasedInput = matchingKeyword?.toLowerCase() || '';
+  const lowerCasedInput = matchingKeyword.toLowerCase();
 
   return matchingField.some((field) => {
     if (typeof matchingTarget[field] === 'string') {
