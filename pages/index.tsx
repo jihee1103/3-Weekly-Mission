77eea095ecf25655cb3b9873f5898a0b53799d0d
Crack_Egg <jaemyung123@naver.com>
2024-02-18 05:20:55 +0900

    Refactor: index.tsx 파일 경로 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import Folder from "@/components/Folder/Folder";
-import Shared from "../components/Shared/Shared";
 
 export default function Home() {
   return (
     <>
       <Folder />
-      <Shared />
     </>
   );
 }
